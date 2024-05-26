38f199021287437e5fcd161ca64d68ca5c871045
Levy Mitsugui <77492685+LevyMitsugui@users.noreply.github.com>
2024-05-26 01:10:52 +0100

    custom commands

--- 5-ERP/client.py
+++ 5-ERP/client.py
@@ -2,19 +2,20 @@ import socket
 import time
 from xml.dom import minidom
 
-file = minidom.parse('II_comands_2023-2024_v1/command1.xml')
-print(file.toprettyxml())
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+
+
+""" file = minidom.parse('II_comands_2023-2024_v1/command1.xml')
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.sendto(file.toprettyxml().encode(), (HOST, PORT))
     data, addr = s.recvfrom(1024)
 
 print('Received from', addr)
-print(minidom.parseString(data).toprettyxml())
+print(minidom.parseString(data).toprettyxml()) """
 
-input()
+""" input()
 
 file = minidom.parse('II_comands_2023-2024_v1/command2a.xml')
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -44,12 +45,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 print('Received from', addr)
 print(minidom.parseString(data).toprettyxml())
 
-input()
+input() """
 
-file = minidom.parse('II_comands_2023-2024_v1/command4.xml')
+""" file = minidom.parse('II_comands_2023-2024_v1/command4.xml')
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.sendto(file.toprettyxml().encode(), (HOST, PORT))
     data, addr = s.recvfrom(1024)
+ 
+print('Received from', addr)
+print(minidom.parseString(data).toprettyxml()) """
 
+
+""" file = minidom.parse('II_comands_2023-2024_v1/command6.xml')
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.sendto(file.toprettyxml().encode(), (HOST, PORT))
+    data, addr = s.recvfrom(1024)
+ 
+print('Received from', addr)
+print(minidom.parseString(data).toprettyxml()) """
+
+file = minidom.parse('II_comands_2023-2024_v1/command7.xml')
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.sendto(file.toprettyxml().encode(), (HOST, PORT))
+    data, addr = s.recvfrom(1024)
+ 
 print('Received from', addr)
 print(minidom.parseString(data).toprettyxml())
